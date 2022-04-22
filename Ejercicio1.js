6d88635452034430faad07b87845d82344613a7a
Daniel Castillejo <102350282+Plaracil@users.noreply.github.com>
2022-04-22 09:34:24 +0200

    Update Ejercicio1.js

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,1 +1,18 @@
+function Mayor(){
 
+    let number1, number2, mayor;
+    
+    number1 = parseInt(document.getElementById("number1").value);
+    number2 = parseInt(document.getElementById("number2").value);
+
+    if(number1 > number2){
+
+        alert("El número " + number1 + " es mayor que el número " + number2)
+        
+
+    }else{
+
+        alert("El número " + number2 + " es mayor que el número " + number1)
+       
+    }
+}
