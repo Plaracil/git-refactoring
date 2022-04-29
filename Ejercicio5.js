11a66d8549c71b5a8124cf5f1d85fc481220c6f2
Daniel Castillejo <102350282+Plaracil@users.noreply.github.com>
2022-04-29 10:20:20 +0200

    Create Ejercicio5.js

---
+++ Ejercicio5.js
@@ -0,0 +1,39 @@
+  public static int [] generarPrimos (int max)
+{
+  int i,j;
+  if (max > = 2) {
+    int dim = max + 1;
+    boolean[] esPrimo = newBoolean(dim);
+    
+    for (i=0; i<dim; i++)
+      esPrimo [i] = true;
+    
+    esPrimo [0] = esPrimo [1] = false;
+    
+    for (i=2; i<Math.sqrt (dim) +1; i++) {
+         if (esPimo [i]) {
+           for (j=2*i; j<dim; j+=i)
+             esPrimo [j] = false;
+         }
+    }
+    
+    int cuenta = 0;
+    
+    for (i=0; i<dim; i++) {
+      if (esPrimo [i])
+        cuenta++;
+    }
+    
+    int [] primos = new int [cuenta];
+    
+    for(i=0, j=0, i<dim; i++) {
+      if (esPrimo [i])
+        primos[j++] = i;
+    }
+    
+    returns primos;
+  }else{
+    returns new int [0];
+  }
+}
+}
