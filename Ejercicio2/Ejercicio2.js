aca0f9ceef2435381d1b67f23078c05b89de90c3
Daniel Castillejo <102350282+Plaracil@users.noreply.github.com>
2022-04-29 09:08:20 +0200

    Create Ejercicio2.js

---
+++ Ejercicio2/Ejercicio2.js
@@ -0,0 +1,28 @@
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+
+if (fecha1>=fecha2){
+            //If anidado
+            if(fecha1==fecha2){
+                System.out.println("La fecha "+fecha1+" y "+num2+" son iguales");
+            }else{
+                System.out.println("La fecha "+fecha1+" es mayor que la fecha2 "+fecha2);
+            }
+        }else{
+            System.out.println("La fecha "+num2+" es mayor que la fecha "+num1);
+        }
+    }
+}
+
+alert(meses[0]);
+alert(meses[1]);
+alert(meses[2]);
+alert(meses[3]);
+alert(meses[4]);
+alert(meses[5]);
+alert(meses[6]);
+alert(meses[7]);
+alert(meses[8]);
+alert(meses[9]);
+alert(meses[10]);
+alert(meses[11]);
